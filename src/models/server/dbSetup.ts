0ef707fc523bad1da8db3e13499b2c99c1a17c43
Gobinda Das <dasgobinda9955@gmail.com>
2025-04-30 12:00:57 +0530

    database setup completed

---
+++ src/models/server/dbSetup.ts
@@ -0,0 +1,40 @@
+import { db } from "../name";
+import createQuestionCollection from "./question.collection";
+import createAnswerCollection from "./answer.collection";
+import createCommentCollection from "./comment.collection";
+import createVoteCollection from "./vote.collection";
+
+import { databases } from "./config";
+
+
+
+export default async function getOrCreateDB() {
+    try {
+        await databases.get(db);
+        console.log("Database connected");
+    } catch (error) {
+        try {
+            await databases.create(db, db);
+
+            console.log("Database created");
+            
+
+            // Create collections
+            await Promise.all([
+                createQuestionCollection(),
+                createAnswerCollection(),
+                createCommentCollection(),
+                createVoteCollection(),
+            ]);
+            
+            console.log("Collections created");
+            console.log("Database connected");
+            
+        } catch (error) {
+            console.log("Error while creating database or collections: ", error);
+        }
+    }
+
+
+    return databases;
+}
