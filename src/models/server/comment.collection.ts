e2b4b290b9709312d950ab46bdc504fb128194e4
Gobinda Das <dasgobinda9955@gmail.com>
2025-04-30 11:24:20 +0530

    add comment-collection

---
+++ src/models/server/comment.collection.ts
@@ -0,0 +1,28 @@
+import { Permission } from "node-appwrite";
+import { db, commentCollection } from "../name";
+import { databases } from "./config";
+
+
+
+export default async function createAnswerCollection() {
+    
+    await databases.createCollection(db, commentCollection, "comments", [
+        Permission.read("any"),
+        Permission.read("users"),
+        Permission.create("users"),
+        Permission.update("users"),
+        Permission.delete("users"),
+    ])
+
+    console.log("Comment collection created");
+
+
+    await Promise.all([
+        databases.createStringAttribute(db, commentCollection, "content", 10000, true),
+        databases.createStringAttribute(db, commentCollection, "type", ["answer", "question"], true),
+        databases.createStringAttribute(db, commentCollection, "typeId", 100, true),
+        databases.createStringAttribute(db, commentCollection, "authorId", 100, true),
+    ])
+
+    console.log("Comment attributes created");
+}
